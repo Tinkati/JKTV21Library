d4ad8fa38d127eb35b25d2365c9b073e6def135b
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2022-10-10 11:36:30 +0300

    Создали класс App с пользовательским интерфейсом

--- src/jktv21library/App.java
+++ src/jktv21library/App.java
@@ -5,6 +5,9 @@
  */
 package jktv21library;
 
+import entity.Book;
+import entity.History;
+import entity.Reader;
 import java.util.Scanner;
 
 /**
@@ -18,12 +21,42 @@ public class App {
         do{
             System.out.println("Функции приложения:");
             System.out.println("0. Закрыть приложение");
+            System.out.println("1. Добавить книгу");
+            System.out.println("2. Добавить читателя");
+            System.out.println("3. Выдать книгу");
+            System.out.println("4. Вернуть книгу");
+            System.out.println("5. Список книг");
+            System.out.println("6. Список авторов");
             System.out.print("Выберите номер функции: ");
             int task = scanner.nextInt();
             scanner.nextLine();
             switch (task) {
                 case 0:
                     repeat = false;
+                    break;
+                case 1:
+                    System.out.println("1. Добавить книгу");
+                    Book book = new Book();
+                    
+                    break;
+                case 2:
+                    System.out.println("2. Добавить читателя");
+                    Reader reader = new Reader();
+                    break;
+                case 3:
+                    System.out.println("3. Выдать книгу");
+                    History history = new History();
+                    break;
+                case 4:
+                    System.out.println("4. Вернуть книгу");
+                    
+                    break;
+                case 5:
+                    System.out.println("5. Список книг");
+                    break;
+                case 6:
+                    System.out.println("6. Список авторов");
+                    
                     break;
                 default:
                     System.out.println("Выберите номер функции из списка!");
